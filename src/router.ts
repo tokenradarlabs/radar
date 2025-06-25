a9810cb30c8e2d8be308e09906ee509708d2c470
Scott Rose <mrscottyrose@gmail.com>
2025-06-25 05:03:01 -0600

    Add priceChangeController for fetching token price changes
    - Introduced priceChangeController to handle GET requests for token price changes at the endpoint /api/v1/priceChange/:tokenId.
    - Integrated fetchTokenPriceChange utility to retrieve 24h price change data from the CoinGecko API.
    - Implemented error handling for cases where token price change data is not found or if the fetch operation fails.

--- src/router.ts
+++ src/router.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import userController from "./controller/userController";
 import indexController from "./controller/indexController";
 import priceController from "./controller/priceController";
+import priceChangeController from "./controller/priceChangeController";
 
 export default async function router(fastify: FastifyInstance) {
   fastify.register(userController, { prefix: "/api/v1/user" });
   fastify.register(indexController, { prefix: "/" });
   fastify.register(priceController, { prefix: "/api/v1/price" });
+  fastify.register(priceChangeController, { prefix: "/api/v1/priceChange" });
 }
