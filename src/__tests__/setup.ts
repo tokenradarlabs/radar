07538432e2bc7cc9e235de72619b11f887c7ef4c
Ronald Gene <regenify.base.eth@gmail.com>
2025-10-17 23:29:50 +0000

    feat: Add token aliases for price service

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,8 +1,6 @@
 import { beforeAll, afterAll } from 'vitest';
-import dotenv from 'dotenv';
 
 // Load test environment variables
-dotenv.config({ path: '.env.test' });
 
 beforeAll(async () => {
   // Global test setup
