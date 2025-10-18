799c05d9d06f53f257e68db09078aae2825887c5
Alison Hawk <185055428+alisonhawk@users.noreply.github.com>
2025-10-18 16:30:28 +0600

    Refactor: Consolidate priceTokenIdSchema definition (#120)

--- src/lib/api/price/price.schema.ts
+++ src/lib/api/price/price.schema.ts
@@ -1,19 +1,5 @@
 import { z } from 'zod';
-
-export const priceTokenIdSchema = z.object({
-  tokenId: z
-    .string({
-      required_error: 'Token ID is required',
-      invalid_type_error: 'Token ID must be a string',
-    })
-    .refine(
-      (tokenId) => ['btc', 'eth', 'scout-protocol-token'].includes(tokenId),
-      {
-        message:
-          'Invalid token selection. Supported tokens are: btc, eth, scout-protocol-token',
-      }
-    ),
-});
+export { priceTokenIdSchema } from '../../../utils/validation';
 
 export interface TokenPriceParams {
   tokenId: string;
